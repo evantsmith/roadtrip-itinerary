1a9a62dfe3b9b2bb46fb6c05452dd58f62e43253
Evan Smith <evansmith@Evans-MacBook-Pro.local>
2017-10-31 22:49:09 -0600

    final commit

--- server-roadtrip-itinerary.js
+++ server-roadtrip-itinerary.js
@@ -2,9 +2,28 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static('./public/css'));
+
 app.get('/San-Francisco', function(request,response){
     response.sendFile('./public/html/sf-1.html',{root: './'});
 });
+
+app.get('/Boston', function(request,response){
+    response.sendFile('./public/html/bos-2.html',{root: './'});
+});
+
+app.get('/New-York-City', function(request,response){
+    response.sendFile('./public/html/ny-3.html',{root: './'});
+});
+
+app.get('/Los-Angelos', function(request,response){
+    response.sendFile('./public/html/la-4.html',{root: './'});
+});
+
+app.get('/Miami', function(request,response){
+    response.sendFile('./public/html/miami-5.html',{root: './'});
+});
+
 
 app.listen(8080, function(){
     console.log('The app is running on port 8080');
